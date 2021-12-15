022290efbadb445a250829bd5cd0ae98f0cf4a23
wahmd <wahmdgit@gmail.com>
2021-12-15 23:29:04 +0500

    add handleOrder Lambda

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,10 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "handleOrder": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
