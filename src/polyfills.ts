3693278f425f57206cbb6fb07c4d0847de33a0ad
wahmd <wahmdgit@gmail.com>
2021-12-15 23:03:56 +0500

    :rocket: amplify init

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,3 +51,11 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+(window as any).global = window;
+(window as any).process = {
+    env: { DEBUG: undefined },
+};
+
+import 'core-js/es/typed-array';
+import 'core-js/es/object';
